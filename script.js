9d876f634861fbb961ac038211a37ce8d8e9438a
sonamycse25-glitch <sonamy.cse25@sbjit.edu.in>
2026-04-29 22:20:46 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Bootstrap JS Load
+let bootstrapScript = document.createElement("script");
+bootstrapScript.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js";
+document.body.appendChild(bootstrapScript);
