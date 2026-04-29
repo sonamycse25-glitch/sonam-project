15efaa9377f8010199dfc6fb38d55d83b90da38e
sonamycse25-glitch <sonamy.cse25@sbjit.edu.in>
2026-04-29 23:12:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Bootstrap JS Load
-let bootstrapScript = document.createElement("script");
-bootstrapScript.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js";
-document.body.appendChild(bootstrapScript);
+
+var script = document.createElement("script");
+script.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js";
+document.body.appendChild(script);
